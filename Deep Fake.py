b43814024f22d8d25be5a4378dd20f4d4cbfbfb8
Esaeon <145063995+Esaeon@users.noreply.github.com>
2023-10-08 13:39:37 -0500

    Interface Completion
    I have pretty much gotten the interface figured out. I did use another python file, but it's okay since it doesn't require an installation on every computer. Also, I've made it so that only JPG files work for now.
    
    Later on, I'll start working on the graphics window to display the images.

--- Deep Fake.py
+++ Deep Fake.py
@@ -1,7 +1,10 @@
-from PIL import Image
 from graphics import *
+from filedialog import *
+import time
 
 def main():
+    proceed = False
+    
     interfaceWindow = GraphWin("Deep Fake v0.1.1", 1000, 500)
     imageChooser1 = Rectangle(Point(100, 100), Point(900, 200))
     imageChooser1.draw(interfaceWindow)
@@ -19,12 +22,51 @@ def main():
     proceedRec.draw(interfaceWindow)
     proceedText = Text(Point(500, 425), "Proceed")
     proceedText.draw(interfaceWindow)
-    
-##    imageFile = input("Please paste the image path location: ")
-##    imageOriginal = Image.open(imageFile)
-##
-##    imageFile2 = input("Please paste the image path location: ")
-##    imageOriginal2 = Image.open(imageFile2)
+
+    imageBackground = ""
+    imageFace = ""
+    while (proceed == False):
+        point = interfaceWindow.getMouse()
+        pointX = point.getX()
+        pointY = point.getY()
+        print(point)
+
+        if ((pointX<900) & (pointX>100)):
+            if ((pointY<200) & (pointY>100)):
+                if (len(imageBackground)!=0):
+                    imageBackgroundText.undraw()
+                imageBackground = askopenfilename()
+                imageBackgroundText = Text(Point(500,220), imageBackground)
+                imageBackgroundText.draw(interfaceWindow)
+
+            elif ((pointY<350) & (pointY>250)):
+                if (len(imageFace)!=0):
+                    imageFaceText.undraw()
+                imageFace = askopenfilename()
+                imageFaceText = Text(Point(500,370), imageFace)
+                imageFaceText.draw(interfaceWindow)
+
+            else: time.sleep(1)
+
+        if (((pointX<700) & (pointX>300))& ((pointY<450) & (pointY>400))):
+            if ((len(imageBackground)!=0) & (len(imageFace)!=0)):
+                if (imageBackground.endswith("JPG") & imageFace.endswith("JPG")):
+                    proceed = True
+                else:
+                    ErrorMessage = "Files other than JPG are not supported in this iteration."
+                    ErrorMessageText = Text(Point(500, 470), ErrorMessage)
+                    ErrorMessageText.draw(interfaceWindow)
+                    time.sleep(5)
+                    ErrorMessageText.undraw()
+                
+            else:
+                ErrorMessage = "Please choose two files."
+                ErrorMessageText = Text(Point(500, 470), ErrorMessage)
+                ErrorMessageText.draw(interfaceWindow)
+                time.sleep(5)
+                ErrorMessageText.undraw()
+
+    interfaceWindow.close()
     
 
 
